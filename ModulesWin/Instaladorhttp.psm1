56d123370edda6dec4f846ef392b1a8399eb2ca6
aleee-lc <el_alex_2004@hotmail.com>
2025-03-18 17:05:29 -0700

    .

--- ModulesWin/Instaladorhttp.psm1
+++ ModulesWin/Instaladorhttp.psm1
@@ -5,10 +5,8 @@ function Instalar-ServicioWeb {
     Write-Host "1) Nginx"
     Write-Host "2) IIS"
     Write-Host "3) Lighttpd"
-    $servicio = Read-Host "Selecciona el servicio a instalar (1-3)"
 
-    $puerto = Read-Host "Ingresa el puerto a configurar"
-    $ssl = Read-Host "¿Deseas habilitar SSL? (s/n)"
+    $servicio = Read-Host "Selecciona el servicio a instalar (1-3)"
 
     if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
         Write-Host "Chocolatey no encontrado. Instalando..." -ForegroundColor Yellow
@@ -20,20 +18,20 @@ function Instalar-ServicioWeb {
     switch ($servicio) {
         "1" {
             choco install nginx -y
-            Start-Process -FilePath "C:\tools\nginx\nginx.exe"
+            Start-Process -FilePath "C:\\tools\\nginx\\nginx.exe"
             Write-Host "✅ NGINX iniciado correctamente" -ForegroundColor Green
         }
         "2" {
-            Write-Host "Instalando IIS..."
+            Write-Host "Instalando IIS..." -ForegroundColor Yellow
             Install-WindowsFeature -name Web-Server -IncludeManagementTools
             Write-Host "✅ IIS instalado correctamente" -ForegroundColor Green
         }
         "3" {
-            Write-Host "Instalando Lighttpd..."
+            Write-Host "Instalando Lighttpd..." -ForegroundColor Yellow
             choco install lighttpd -y
             Write-Host "✅ Lighttpd instalado correctamente" -ForegroundColor Green
         }
-        default {
+        Default {
             Write-Host "Opción no válida" -ForegroundColor Red
         }
     }
--- sslhttp.ps1
+++ sslhttp.ps1
@@ -1,3 +1,5 @@
 Import-Module "$PSScriptRoot\ModulesWin\Instaladorhttp.psm1" -Force
+Import-Module "$PSScriptRoot\ModulesWin\ValidarUser.psm1" -Force
 
+Validar-Admin
 Instalar-ServicioWeb
